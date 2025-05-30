5b0f4d61ad892caa9e896704ab57ee261006c00d
Karin Wu <karin.wu@hotmail.com>
2025-05-29 23:18:31 -0500

    add Home.py

---
+++ intro_stream/streamlit_app/Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Self Introduction",
+    page_icon="🏠",
+    layout="wide"
+)
+
+st.header("About Me")
+st.subheader("Name")
+st.write("Karin Wu")
+st.subheader("Education")
+st.write("MSc in Data Science, Georgia Tech")
