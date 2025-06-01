640b1fdb1e535a75ff1d1fe4bb5971cb91d58bc3
Karin Wu <71893234+karinwu@users.noreply.github.com>
2025-05-31 23:28:57 -0500

    add values from constants file

--- intro_stream/streamlit_app/pages/2_Passion.py
+++ intro_stream/streamlit_app/pages/2_Passion.py
@@ -1,19 +1,34 @@
 import streamlit as st
+import constants as c
 
 st.set_page_config(
     page_title="2. Passion",
     page_icon="❤️",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
-st.header("My Passions")
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.subheader("🍰 Cake")
-    st.write("I love baking and decorating cakes.")
-with col2:
-    st.subheader("🏊‍♀️ Swimming")
-    st.write("Swimming keeps me active and refreshed.")
-with col3:
-    st.subheader("🌱 Renewable Energy")
-    st.write("I'm passionate about sustainable energy solutions.")
+margin_r, body, margin_l = st.columns([0.4, 3, 0.4])
+
+
+with body:
+    c.menu()
+
+    st.subheader("🍰 Cakes and Cookies", divider='rainbow')
+    col1, col2, col3 = st.columns([1.3, 0.2, 1])
+    with col1:
+        st.markdown(f"###### {c.passions['cakes']}")
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    st.subheader("🏊‍♀️ Swimming", divider='rainbow')
+    col1, col2, col3 = st.columns([1.3, 0.2, 1])
+    with col1:
+        st.markdown(f"###### {c.passions['swimming']}")
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    st.subheader("🌱 Renewable Energy", divider='rainbow')    
+    st.markdown(f"###### {c.passions['renewable_energy']}")
+    st.markdown(f" - {c.passions['project']}")
+    st.markdown(f" - {c.passions['bess']}")
